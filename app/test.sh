95ef7e67b884e86e19691b07b2f8d0835bd1b4dd
ldynia <l_dynowski@yahoo.com>
2021-03-15 09:39:47 +0100

    Adding unit test pipeline

--- app/test.sh
+++ app/test.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
+# Define vars
+NC='\033[0m'
+RED='\033[0;31m'
+BLUE='\e[36m'
+YELLOW='\033[0;33m'
 EXIT_CODE=0
 
-rm -rf /app/coverage.log
+# Clean coverage related files
 rm -rf /app/.coverage
-rm -rf /app/test.log
+rm -rf /app/coverage.log
+rm -rf /app/htmlcov
+rm -rf /tmp/test.log
 
+printf "${YELLOW}Testing Started${NC}\n"
+
+# Run tests via test coverage
 coverage erase
-coverage run manage.py test &> test.log
+coverage run /app/manage.py test > /tmp/test.log 2>&1
 coverage report 2>&1 | tee coverage.log
-
-# Define colors
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-BLUE='\e[36m'
-NC='\033[0m'
+coverage html
+coverage xml
 
 # Check if unit tests failed!
-TEST_RESULT=$(cat test.log  | grep -o 'FAILED')
+TEST_RESULT=$(cat /tmp/test.log  | grep -o 'FAILED')
 if [ $TEST_RESULT ]; then
     printf "${RED}ERROR:${NC} ${YELLOW}Test $TEST_RESULT!${NC}\n"
     printf "${BLUE}"
-    cat test.log
+    cat /tmp/test.log
     printf "${NC}"
     EXIT_CODE=1
 fi
@@ -35,8 +41,11 @@ if [ "$COVERAGE_RESULT" = "failure" ]; then
     EXIT_CODE=1
 fi
 
-rm -rf /app/coverage.log
 rm -rf /app/.coverage
-rm -rf /app/test.log
+rm -rf /app/coverage.log
+rm -rf /app/htmlcov
+rm -rf /tmp/test.log
+
+printf "${YELLOW}Testing Ended${NC}\n"
 
 exit $EXIT_CODE
